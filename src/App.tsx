98973d51ca2b914d9d5c53c01d074b818e110ffc
Muhammad Nauman <nauman33183@gmail.com>
2024-09-19 01:47:01 -0700

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
-import Timer from "./pagecomponents/Timer";
+import Timer from "./pagecomponents/timer";
 import Calculator from "./pages/Calculator";
 import { Routes, Route } from "react-router-dom";
 import useWeb3 from "./web3/webConfig/web3";
